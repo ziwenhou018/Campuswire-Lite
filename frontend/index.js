7f20d2771a4a1c524e8abd8d5003bcb1a8457894
ziwenhou018 <ziwenhou018@gmail.com>
2021-11-21 23:21:36 -0500

    created a front/back end

---
+++ frontend/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <h1>Your react app!</h1>
+
+export default App
---
+++ frontend/src/App.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const App = () => <h1>Your react app!</h1>
+export default App
